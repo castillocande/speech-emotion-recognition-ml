6ba632bb33cda8694abdae8a2dccd0e9bd694133
castillocande <castilloc@udesa.edu.ar>
2024-06-21 12:57:55 -0300

    new files

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 from src.pipeline import run_experiment
-# from src.data_loader import load_data
 from src.files import read_configs
 
-def main(): # recibe los configs como parámetro
-    # tenemos que hacer que, cada vez que agregamos una config, 
-    # se nos debería agregar sola una carpeta con los resultados. 
-    # esto lo deberíamos hacer e el main. 
 
-    # recibe los nombres de los archivos?
-    # dataset = load_data(config_file=r"configs\data\[el nombre del .py]")
-    # features = get_features(dataset, config_file=r"configs\features") 
-    # con open smile, obtenemos directamente los features, no solo el dataset. cómo hacemos eso?
+def main(): 
     model_config, data_config, features_config = read_configs()
-    # al medio defino el logger
-    run_experiment(model_config, data_config, features_config) #paths, configuraciones del modelo y de las features que quiero extraer
+    run_experiment(model_config, data_config, features_config) 
+
 
 if __name__ == "__main__":
     main()
