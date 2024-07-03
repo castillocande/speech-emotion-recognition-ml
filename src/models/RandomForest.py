07c1fa830d86ac43412885f8029e365b0b29b163
MateoCostantini <costantinim@udesa.edu.ar>
2024-07-02 23:14:37 -0300

    cambios lstm, CV randomForest, mas segmentos en muestras

--- src/models/RandomForest.py
+++ src/models/RandomForest.py
@@ -1,2 +1,47 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
+class RandomForest:
+    def __init__(self, n_estimators=100, max_features='auto', max_depth=None,
+                 min_samples_split=2, min_samples_leaf=1, bootstrap=True,
+                 criterion='gini', random_state=42):
+        self.n_estimators = n_estimators
+        self.max_features = max_features
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.min_samples_leaf = min_samples_leaf
+        self.bootstrap = bootstrap
+        self.criterion = criterion
+        self.random_state = random_state
+        self.model = RandomForestClassifier(
+            n_estimators=self.n_estimators,
+            max_features=self.max_features,
+            max_depth=self.max_depth,
+            min_samples_split=self.min_samples_split,
+            min_samples_leaf=self.min_samples_leaf,
+            bootstrap=self.bootstrap,
+            criterion=self.criterion,
+            random_state=self.random_state
+        )
+
+    def fit(self, X_train, y_train, X_valid, y_valid):
+        X_train_shape = X_train.shape
+        X_valid_shape = X_valid.shape
+        if len(X_train_shape) == 3:
+            X_train = X_train.reshape(-1, X_train_shape[-1])
+            X_valid = X_valid.reshape(-1, X_valid_shape[-1])
+        self.model.fit(X_train, y_train)
+        val_predictions = self.model.predict(X_valid)
+        #X_train = X_train.reshape(X_train_shape[0], X_train_shape[1], X_train_shape[2])
+        #X_test = X_test.reshape(X_test_shape[0], X_test_shape[1], X_test_shape[2])
+        acc = accuracy_score(y_valid, val_predictions)
+        print(f"Validation Accuracy: {acc}")
+        return acc
+
+
+
+    def predict(self, X):
+        X_shape = X.shape
+        if len(X_shape) == 3:
+            X = X.reshape(-1, X_shape[-1])
+        return self.model.predict(X)
--- src/models/lstm.py
+++ src/models/lstm.py
@@ -1,7 +1,7 @@
 from keras.models import Model, Sequential
 from tensorflow.keras.layers import LSTM, Dropout, Dense
 from tensorflow.keras.optimizers import Adam, SGD
-from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
@@ -28,6 +28,7 @@ class rnnLSTM():
         test_loss, test_accuracy = self.model.evaluate(X_test, y_test)
         return test_loss, test_accuracy
 """
+"""
 class rnnLSTM():
     def __init__(self, X_train, y_train, lr=0.001, patience=3, dropout_rate=0.5, lstm_units=128, momentum = 0.9):
         self.early_stopping = EarlyStopping(monitor='val_loss', patience=patience, restore_best_weights=True)
@@ -49,6 +50,113 @@ class rnnLSTM():
         test_loss, test_accuracy = self.model.evaluate(X_test, y_test)
         return test_loss, test_accuracy
 
+    def plot_learning_curves(self):
+        history_dict = self.history.history
+        loss = history_dict['loss']
+        val_loss = history_dict['val_loss']
+        accuracy = history_dict['accuracy']
+        val_accuracy = history_dict['val_accuracy']
+        
+        epochs = range(1, len(loss) + 1)
+        
+        plt.figure(figsize=(14, 5))
+        
+        plt.subplot(1, 2, 1)
+        plt.plot(epochs, loss, 'bo', label='Training loss')
+        plt.plot(epochs, val_loss, 'b', label='Validation loss')
+        plt.title('Training and validation loss')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.legend()
+        
+        plt.subplot(1, 2, 2)
+        plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
+        plt.plot(epochs, val_accuracy, 'b', label='Validation accuracy')
+        plt.title('Training and validation accuracy')
+        plt.xlabel('Epochs')
+        plt.ylabel('Accuracy')
+        plt.legend()
+        
+        plt.show()
+"""
+"""
+class rnnLSTM():
+    def __init__(self, X_train, y_train, lr=0.01, momentum=0.9, patience=3, dropout_rate=0.6, lstm_units=128, regularization=0.01):
+        self.early_stopping = EarlyStopping(monitor='val_loss', patience=patience, restore_best_weights=True)
+        self.reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=patience//2, min_lr=1e-5)
+        self.model = Sequential()
+        self.model.add(LSTM(lstm_units, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])))
+        self.model.add(Dropout(dropout_rate))
+        self.model.add(LSTM(lstm_units, return_sequences=True))
+        self.model.add(Dropout(dropout_rate))
+        self.model.add(LSTM(lstm_units, return_sequences=False))
+        self.model.add(Dropout(dropout_rate))
+        self.model.add(Dense(256, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(regularization)))
+        self.model.add(Dropout(dropout_rate))
+        self.model.add(Dense(128, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(regularization)))
+        self.model.add(Dropout(dropout_rate))
+        self.model.add(Dense(y_train.shape[1], activation='softmax'))
+        self.model.compile(optimizer=SGD(learning_rate=lr, momentum=momentum), loss='categorical_crossentropy', metrics=['accuracy'])
+
+    def train(self, X_train, y_train, X_valid, y_valid, epochs=50, batch_size=32, random_seed=42):
+        tf.random.set_seed(random_seed)
+        self.history = self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_valid, y_valid), callbacks=[self.early_stopping, self.reduce_lr])
+
+    def evaluate(self, X_test, y_test):
+        test_loss, test_accuracy = self.model.evaluate(X_test, y_test)
+        return test_loss, test_accuracy
+
+    def plot_learning_curves(self):
+        history_dict = self.history.history
+        loss = history_dict['loss']
+        val_loss = history_dict['val_loss']
+        accuracy = history_dict['accuracy']
+        val_accuracy = history_dict['val_accuracy']
+        
+        epochs = range(1, len(loss) + 1)
+        
+        plt.figure(figsize=(14, 5))
+        
+        plt.subplot(1, 2, 1)
+        plt.plot(epochs, loss, 'bo', label='Training loss')
+        plt.plot(epochs, val_loss, 'b', label='Validation loss')
+        plt.title('Training and validation loss')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.legend()
+        
+        plt.subplot(1, 2, 2)
+        plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
+        plt.plot(epochs, val_accuracy, 'b', label='Validation accuracy')
+        plt.title('Training and validation accuracy')
+        plt.xlabel('Epochs')
+        plt.ylabel('Accuracy')
+        plt.legend()
+        
+        plt.show()
+"""
+class rnnLSTM():
+    def __init__(self, X_train, y_train, lr = 0.001, patience = 3, dropout_rate = 0.5, momentum = 0.9):
+        self.early_stopping = EarlyStopping(monitor='val_loss', patience=patience, restore_best_weights=True)
+        self.model = Sequential()
+        self.model.add(LSTM(128, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])))
+        self.model.add(Dropout(dropout_rate))
+        self.model.add(LSTM(128, return_sequences=False))
+        self.model.add(Dropout(dropout_rate))
+        self.model.add(Dense(128, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01)))
+        self.model.add(Dropout(dropout_rate))
+        self.model.add(Dense(y_train.shape[1], activation='softmax'))
+        self.model.compile(optimizer=SGD(learning_rate=lr, momentum=momentum), loss='categorical_crossentropy', metrics=['accuracy'])
+
+    def train(self, X_train, y_train, X_valid, y_valid, epochs=10, batch_size=32, random_seed=42):
+        print("cambio")
+        tf.random.set_seed(random_seed)
+        self.history = self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_valid, y_valid), callbacks=[self.early_stopping])
+
+    def evaluate(self, X_test, y_test):
+        test_loss, test_accuracy = self.model.evaluate(X_test, y_test)
+        return test_loss, test_accuracy
+    
     def plot_learning_curves(self):
         history_dict = self.history.history
         loss = history_dict['loss']
