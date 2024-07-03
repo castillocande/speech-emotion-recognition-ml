ce1a6cd090c0c4adcc8fb7854052062c2d70098e
castillocande <castilloc@udesa.edu.ar>
2024-07-03 20:21:45 -0300

    versión final

--- src/pipeline.py
+++ src/pipeline.py
@@ -5,7 +5,6 @@ from src.files import create_results_folder
 def load_model(model_config):
     model_module = importlib.import_module(f"src.models.{model_config.name}")
     model = getattr(model_module, model_config.name)(model_config)
-    # model = model.to(model_config.device)
     return model
 
 
@@ -20,7 +19,8 @@ def run_experiment(model_config, data_config, features_config):
         if model_config.name == "RNN":
             pass
         elif model_config.name == "CNN":
-            model.compile_model()
-            model.train(data_config.train_dir)
-            model.evaluate(data_config.test_dir)
+            # model.compile_model()
+            # model.train(data_config.train_dir)
+            # model.evaluate(data_config.test_dir)
+            pass
     
